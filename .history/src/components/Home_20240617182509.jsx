c333131baeac9f3bd1fc4f94919c18eec702ee98
Rakesh Puppala <rakeshpuppala2591.com>
2024-06-17 18:27:49 -0700

    Fixed some UI issues

--- .history/src/components/Home_20240617182509.jsx
+++ .history/src/components/Home_20240617182509.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-import Nav from "./Nav";
-
 const Home = () => {
-  return (
-    <>
-      <Nav />
+    return (
       <div className="home">
         <h1>Home Page</h1>
       </div>
-    </>
-  );
-};
-
+    );
+  };
 export default Home;
+  
